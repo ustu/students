d1692e10cdeba0c22462d11654c865bb42b5a781
Svintsov Dmitry <root@uralbash.ru>
2017-09-27 10:59:09 +0500

    clean up

--- default.nix
+++ default.nix
@@ -9,13 +9,13 @@ let
     name = "rstcheck-3.1";
 
     src = pkgs.fetchurl {
-      url = https://pypi.python.org/packages/29/86/397e91d4ec3638e331a2be493725820deec792ad5077924ed3d15c56faab/rstcheck-3.1.tar.gz;
+      url = mirror://pypi/P/Paste/rstcheck-3.1.tar.gz;
       sha256 = "1s2xxaxnhx6yiax82q2dfi7yxmmzdq5kqcf46cz71hz7g8c2hxq8";
     };
 
     buildInputs = with pythonPackages; [ pythonPackages.docutils ];
 
-    # doCheck = false; # some files required by the test seem to be missing
+    doCheck = false; # some files required by the test seem to be missing
   };
 in rec {
   pyEnv = stdenv.mkDerivation {
